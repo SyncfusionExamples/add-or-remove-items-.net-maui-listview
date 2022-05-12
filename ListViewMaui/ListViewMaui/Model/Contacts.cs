1d9558263ee3ce34ea5cb295139c05a1251a0f89
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2022-05-12 20:20:14 +0530

    Sample added

---
+++ ListViewMaui/ListViewMaui/Model/Contacts.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMaui
+{
+    #region Contacts
+    public class Contacts : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string contactName;
+        private string contactNumber;
+        private ImageSource contactImage;
+
+        #endregion
+
+        #region Constructor
+        public Contacts(string name, string number)
+        {
+            contactName = name;
+            contactNumber = number;
+        }
+
+        public Contacts()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        public string ContactName
+        {
+            get { return contactName; }
+            set
+            {
+                if (contactName != value)
+                {
+                    contactName = value;
+                    this.RaisedOnPropertyChanged("ContactName");
+                }
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set
+            {
+                if (contactNumber != value)
+                {
+                    contactNumber = value;
+                    this.RaisedOnPropertyChanged("ContactNumber");
+                }
+            }
+        }
+
+        public ImageSource ContactImage
+        {
+            get { return contactImage; }
+            set
+            {
+                if (contactImage != value)
+                {
+                    contactImage = value;
+                    this.RaisedOnPropertyChanged("ContactImage");
+                }
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+        #endregion
+    }
+
+    #endregion
+}
